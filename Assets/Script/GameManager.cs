26a0473e5eb4ec8528e6f56f128863c6b8d363a7
hhan87 <hhan87@gmail.com>
2019-01-09 00:51:02 +0900

    insert dots for display score

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,10 +8,39 @@ public class GameManager : MonoBehaviour
 
     public int score1p=0;
     public int score2p=0;
-
+    public int[,] score1pMap = new int[5, 7] {  { 0,0,0,0,1,1,1 },
+                                                { 0,0,0,0,1,0,1 },
+                                                { 0,0,0,0,1,0,1 },
+                                                { 0,0,0,0,1,0,1 },
+                                                { 0,0,0,0,1,1,1 }
+                                                };
+    public int[,] score2pMap = new int[5, 7] {  { 0,0,0,0,1,1,1 },
+                                                { 0,0,0,0,1,0,1 },
+                                                { 0,0,0,0,1,0,1 },
+                                                { 0,0,0,0,1,0,1 },
+                                                { 0,0,0,0,1,1,1 }
+                                                };
     void Awake()
     {
         instance = this;
+        SpriteRenderer dot = GameObject.Find("Dot").GetComponent<SpriteRenderer>();
+        for (int i = 0; i < score1pMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < score1pMap.GetLength(0); j++)
+            {
+                SpriteRenderer dot1 = Instantiate(dot, new Vector3((2f + 0.4f * j), (4f - 0.4f * i), 0f), Quaternion.identity);
+                dot1.name = "dot_1" + i+j;
+            }
+        }
+        for (int i = 0; i < score2pMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < score2pMap.GetLength(0); j++)
+            {
+                SpriteRenderer dot1 = Instantiate(dot, new Vector3((-2f - 0.4f * j), (4f - 0.4f * i), 0f), Quaternion.identity);
+                dot1.name = "dot_2" + i + j;
+            }
+        }
+
     }
     public void scoreUp(int who)
     {
@@ -24,5 +53,18 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        for(int i = 0; i <  score1pMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < score1pMap.GetLength(0); j++)
+            {
+                if(score1pMap[i, j] == 0)
+                {
+
+                }
+            }
+        }
+    }
 
 }
